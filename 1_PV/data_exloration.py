d94c7aae362e39c2021dea10c0393cf2d0102437
a.belotti29 <abelotti00@gmail.com>
2023-08-04 18:24:48 +0200

    data exploration PV

---
+++ 1_PV/data_exloration.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from datetime import datetime, time
+from matplotlib import pyplot as plt
+
+building_consumption = pd.read_csv("1_PV/data/Office building.csv")
+building_production = pd.read_csv("1_PV/data/PV plant.csv")
+
+print(f"building_consumption COLUMNS: {building_consumption.columns}")
+print(f"building_production COLUMNS: {building_production.columns}")
+
+data_series = []
+
+for index, row in building_consumption.iterrows():
+    timestamp_string = row['Timestamp']
+    # Split the string into time and timezone parts
+    time_part, timezone_part = timestamp_string.split("+")
+
+    # Split the time part into hours, minutes, and seconds
+    hours, minutes, seconds = map(int, time_part.split(":"))
+
+    #dt = datetime(row['Year'], row['Month'], row['Day'], hours, minutes, seconds)
+    dt = datetime(row['Year'], row['Month'], row['Day'])
+
+    data_series.append(dt)
+building_consumption['Timestamp'] = data_series
+
+
+avg_power_consum = building_consumption.groupby('Timestamp')['Energy (Wh)'].mean()
+avg_power_consum = (pd.DataFrame(avg_power_consum))
+plt.scatter(avg_power_consum.index, avg_power_consum['Energy (Wh)'], color='blue')
+plt.title('Medium building consumption')
+plt.xlabel('Timestamp')
+plt.ylabel('Energy (Wh)')
+
+data_series = []
+for index, row in building_production.iterrows():
+    timestamp_string = row['Timestamp']
+    # Split the string into time and timezone parts
+    time_part, timezone_part = timestamp_string.split("+")
+
+    # Split the time part into hours, minutes, and seconds
+    hours, minutes, seconds = map(int, time_part.split(":"))
+
+    #dt = datetime(row['Year'], row['Month'], row['Day'], hours, minutes, seconds)
+    dt = datetime(row['Year'], row['Month'], row['Day'])
+
+    data_series.append(dt)
+building_production['Timestamp'] = data_series
+
+avg_power_product = building_production.groupby('Timestamp')['Power (W)'].mean()
+avg_power_product = (pd.DataFrame(avg_power_product))
+avg_power_product['Energy (Wh)'] = avg_power_product['Power (W)']/6
+
+plt.scatter(avg_power_product.index, avg_power_product['Energy (Wh)'], color='blue')
+plt.title('Medium building production')
+plt.xlabel('Timestamp')
+plt.ylabel('Energy (Wh)')
+plt.show()
--- data_exloration.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-building_consumption = pd.read_csv()
-building_production = pd.read_csv()
