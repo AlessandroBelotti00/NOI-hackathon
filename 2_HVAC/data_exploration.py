31ab30531f3cfadb6064987aa608f749c89c87c2
a.belotti29 <abelotti00@gmail.com>
2023-08-04 19:29:17 +0200

    data exp

--- 2_HVAC/data_exploration.py
+++ 2_HVAC/data_exploration.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import json
 
-# energy_temp = pd.read_csv("data/DatiEnergyTemp.csv", encoding='latin-1', error_bad_lines=False)
-# print(energy_temp.shape)  
+energy_temp = pd.read_csv("data/DatiEnergyTemp.csv", delimiter=';')
+# json_file_path = "data/1_dataset.json"
 
-energy_temp = pd.read_json(json_file_path)
+# with open(json_file_path, 'r') as json_file:
+#     data_dict = json.load(json_file)
+
+# columns = list(data_dict[0])[0].split(';')
+
+# energy_temp = pd.DataFrame(columns)
+
+# for el in data_dict:
+#     keys = list(el)[0]
+#     values = el[keys].split(';')
+#     keys = keys.split(';')
+#     result_dict = dict(zip(keys, values))
+
+#     energy_temp = energy_temp.append(result_dict, ignore_index=True)
+
+# print(energy_temp)
+# energy_temp.to_csv('energy_temp_cleaned.csv', index=False)
